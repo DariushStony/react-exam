85436fe798eed19be6d33fad8174979dc761ab6e
Dariush Hadipour <dariushhadi87@gmail.com>
2021-10-05 19:52:47 +0330

    add AboutUs.js

---
+++ src/components/AboutUs.js
@@ -0,0 +1,15 @@
+const AboutUs = () => {
+    return (
+        <div className="about-us">
+            <article>
+                <h2>Dariush Hadipour</h2>
+                <p>Lorem ipusm</p>
+                <div>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Et laudantium autem ratione nesciunt, inventore eveniet aut veritatis porro corrupti iste cum animi consectetur labore natus. Unde esse quos quibusdam maiores?
+                </div>
+            </article>
+        </div>
+    );
+}
+
+export default AboutUs;
