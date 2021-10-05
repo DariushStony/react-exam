ad995205a71239730d61ee9f4b6db0da55c0b90f
Dariush Hadipour <dariushhadi87@gmail.com>
2021-10-05 20:06:13 +0330

    add CardDetails.js

---
+++ src/components/CardDetails.js
@@ -0,0 +1,24 @@
+import { useParams, userHistory } from "react-router";
+import useFetch from "../hooks/useFetch";
+
+const CardDetails = () => {
+
+    const { id } = useParams();
+    const { data: card, isPending, error } = useFetch(`http://localhost/cards/${id}`);
+
+    return (
+        <div className="card-details">
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {card && (
+                <div>
+                    <h2>{card.name}</h2>
+                    <p>This is {card.name} card page.</p>
+                </div>
+            )}
+        </div>
+    );
+
+}
+
+export default CardDetails;
