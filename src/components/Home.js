c1d94a3997db654755f73cacf94611b6b3e43bf0
Dariush Hadipour <dariushhadi87@gmail.com>
2021-10-05 19:09:46 +0330

    update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,17 @@
-
+import CardList from "./CardList";
+import useFetch from "../hooks/useFetch"
 
 const Home = () => {
-    return ( 
+
+    const { data: cards, isPending, error } = useFetch("http://localhost:8000/cards")
+
+    return (
         <div className="home">
-            
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
+            {cards && <CardList cards={cards} title="All Cards!" />}
         </div>
-     );
+    );
 }
 
 export default Home;
