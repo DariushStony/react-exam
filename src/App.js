93d08432925a36783c578e26e77314c94322d1d4
Dariush Hadipour <dariushhadi87@gmail.com>
2021-10-05 18:07:08 +0330

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
     return (
-        <div className="App">
+        <Router>
+            <div className="App">
+                <Navbar />
+            </div>
+        </Router>
 
-        </div>
     );
 }
 
