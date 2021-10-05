3f295991b20fa6f34886a996771b5ae866a3fb50
Dariush Hadipour <dariushhadi87@gmail.com>
2021-10-06 00:35:39 +0330

    update CardDetails.js

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -1,15 +1,17 @@
 import { useParams } from "react-router";
-import useFetch from "../hooks/useFetch";
+// import useFetch from "../hooks/useFetch";
+import { useSelector } from "react-redux";
 
 const CardDetails = () => {
 
     const { id } = useParams();
-    const { data: card, isPending, error } = useFetch(`http://localhost:8000/cards/${id}`);
+    // const { data: card, isPending, error } = useFetch(`http://localhost:8000/cards/${id}`);
+    const card = useSelector((state) => state.find(card => card.id == id));
 
     return (
         <div className="card-details">
-            {isPending && <div>Loading...</div>}
-            {error && <div>{error}</div>}
+            {/* {isPending && <div>Loading...</div>} */}
+            {/* {error && <div>{error}</div>} */}
             {card && (
                 <div>
                     <h2>{card.name}</h2>
